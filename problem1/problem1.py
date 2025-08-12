b952b682280dfe29b4cc759062cc525f06ee9993
morcotkg <karl.morcott@kellogg.northwestern.edu>
2025-08-12 12:19:06 -0500

    enable problem1.py tests

---
+++ problem1/problem1.py
@@ -0,0 +1,11 @@
+def is_leap_year(integer_1):
+    if integer_1 % 4 == 0:
+        if integer_1 % 100 == 0:
+            if integer_1 % 400 == 0:
+                return True
+            else:
+                return False
+        else:
+            return True
+    else:
+        return False
